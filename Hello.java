3bd1c0f8405254bc725f69d45abc2cb0dd7485a0
xb <xb@itcast.cn>
2023-03-02 19:56:45 +0800

    添加hello类

---
+++ Hello.java
@@ -0,0 +1,11 @@
+/**
+@description TODO
+@author xb
+@create 2023-03-02 19:55
+@vesion 1.0
+*/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("你好呀");
+    }
+}
